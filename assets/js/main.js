816c3e731832e9df3d33fcd642617b85ab6bbefe
annu12340 <annujolly17@gmail.com>
2020-04-27 12:20:40 +0530

    added faq, changed img,  refactored code

--- assets/js/main.js
+++ assets/js/main.js
@@ -57,6 +57,19 @@ $(function() {
         });
     });
     
+
+
+
+
+    // ===== FAQ section
+    const items = document.querySelectorAll(".accordion a");
+
+function toggleAccordion(){
+  this.classList.toggle('active');
+  this.nextElementSibling.classList.toggle('active');
+}
+
+items.forEach(item => item.addEventListener('click', toggleAccordion));
     
     //===== wow
     
