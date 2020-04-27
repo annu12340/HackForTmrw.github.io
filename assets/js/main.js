e7108f9afbba4c0b3a1caea6f9f760d8805ac118
annu12340 <annujolly17@gmail.com>
2020-04-27 21:15:29 +0530

    added preloader

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,13 +1,13 @@
+$(window).load(function() {
+  $('.preloader').fadeOut('slow');
+});
 $(function() {
     
     "use strict";
     
     //===== Prealoder
     
-    $(window).on('load', function(event) {
-        $('.preloader').delay(500).fadeOut(500);
-    });
-    
+   
     
     //===== Mobile Menu 
     
